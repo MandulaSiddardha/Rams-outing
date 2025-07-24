96fe4ed99fed3f3ce1f3d4b24c686fea276dfeae
MandulaSiddardha <145456488+MandulaSiddardha@users.noreply.github.com>
2025-07-24 21:03:09 +0530

    Create ram.java

---
+++ ram.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+        int temperature = 50;
+    if(temperature>45){
+        System.out.println("the temperature is "+temperature+"℃");
+        System.out.println("Ram should stay home");
+    }else if(temperature >= 30 && temperature <=45){
+        System.out.println("the temperature is"+temperature+"℃")
+        ;
+        System.out.println("he should carry water");
+    }else if(temperature < 30){
+        System.out.println("the temperature is"+temperature+"℃")
+;
+System.out.println("he should go normal");
+
+    }
+    }
+    }
